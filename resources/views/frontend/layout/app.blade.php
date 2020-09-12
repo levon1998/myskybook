14f5e262120116c82d5bbd850ffc3de6cb74f577
Levon Babayan <levon.babayan.a@gmail.com>
2020-09-13 02:17:56 +0400

    install template for user part

---
+++ resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>@yield('title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/frontend/css/plugins.css') }}" />
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/frontend/css/main.css') }}" />
+        <link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico">
+    </head>
+
+    <body>
+        <div class="site-wrapper" id="top">
+            @include('frontend.layout.header')
+
+            @yield('content')
+        </div>
+        @include('frontend.layout.footer')
+
+        <script src="{{ asset('/frontend/js/plugins.js') }}"></script>
+        <script src="{{ asset('/frontend/js/ajax-mail.js') }}"></script>
+        <script src="{{ asset('/frontend/js/custom.js') }}"></script>
+    </body>
+</html>

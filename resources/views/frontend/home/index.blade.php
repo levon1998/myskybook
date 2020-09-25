d0f5a810dea133f778f803387f5092d0c4f8af91
Levon Babayan <levon.babayan.a@gmail.com>
2020-09-25 21:54:12 +0400

    Customize header

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -6,59 +6,67 @@
 
 @section('content')
 
-    <section class="hero-area hero-slider-1">
-        <div class="sb-slick-slider" data-slick-setting='{
-                            "autoplay": true,
-                            "fade": true,
-                            "autoplaySpeed": 3000,
-                            "speed": 3000,
-                            "slidesToShow": 1,
-                            "dots":true
-                            }'>
-            <div class="single-slide bg-shade-whisper  ">
-                <div class="container">
-                    <div class="home-content text-center text-sm-left position-relative">
-                        <div class="hero-partial-image image-right">
-                            <img src="image/bg-images/home-slider-2-ai.png" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-9">
+                <section class="hero-area hero-slider-1">
+                    <div class="sb-slick-slider" data-slick-setting='{
+                                    "autoplay": true,
+                                    "fade": true,
+                                    "autoplaySpeed": 3000,
+                                    "speed": 3000,
+                                    "slidesToShow": 1,
+                                    "dots":true
+                                    }'>
+                        <div class="single-slide bg-shade-whisper  ">
+                            <div class="container">
+                                <div class="home-content text-center text-sm-left position-relative">
+                                    <div class="hero-partial-image image-right">
+                                        <img src="image/bg-images/home-slider-2-ai.png" alt="">
+                                    </div>
+                                    <div class="row no-gutters ">
+                                        <div class="col-xl-6 col-md-6 col-sm-7">
+                                            <div class="home-content-inner content-left-side">
+                                                <h1>H.G. Wells<br>
+                                                    De Vengeance</h1>
+                                                <h2>Cover Up Front Of Books and Leave Summary</h2>
+                                                <a href="shop-grid.html" class="btn btn-outlined--primary">
+                                                    $78.09 - Order Now!
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="row no-gutters ">
-                            <div class="col-xl-6 col-md-6 col-sm-7">
-                                <div class="home-content-inner content-left-side">
-                                    <h1>H.G. Wells<br>
-                                        De Vengeance</h1>
-                                    <h2>Cover Up Front Of Books and Leave Summary</h2>
-                                    <a href="shop-grid.html" class="btn btn-outlined--primary">
-                                        $78.09 - Order Now!
-                                    </a>
+                        <div class="single-slide bg-ghost-white">
+                            <div class="container">
+                                <div class="home-content text-center text-sm-left position-relative">
+                                    <div class="hero-partial-image image-left">
+                                        <img src="image/bg-images/home-slider-1-ai.png" alt="">
+                                    </div>
+                                    <div class="row align-items-center justify-content-start justify-content-md-end">
+                                        <div class="col-lg-6 col-xl-7 col-md-6 col-sm-7">
+                                            <div class="home-content-inner content-right-side">
+                                                <h1>J.D. Kurtness <br>
+                                                    De Vengeance</h1>
+                                                <h2>Cover Up Front Of Books and Leave Summary</h2>
+                                                <a href="shop-grid.html" class="btn btn-outlined--primary">
+                                                    $78.09 - Learn More
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="single-slide bg-ghost-white">
-                <div class="container">
-                    <div class="home-content text-center text-sm-left position-relative">
-                        <div class="hero-partial-image image-left">
-                            <img src="image/bg-images/home-slider-1-ai.png" alt="">
-                        </div>
-                        <div class="row align-items-center justify-content-start justify-content-md-end">
-                            <div class="col-lg-6 col-xl-7 col-md-6 col-sm-7">
-                                <div class="home-content-inner content-right-side">
-                                    <h1>J.D. Kurtness <br>
-                                        De Vengeance</h1>
-                                    <h2>Cover Up Front Of Books and Leave Summary</h2>
-                                    <a href="shop-grid.html" class="btn btn-outlined--primary">
-                                        $78.09 - Learn More
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </section>
             </div>
         </div>
-    </section>
+    </div>
+
     <!--=================================
     Home Features Section
     ===================================== -->
